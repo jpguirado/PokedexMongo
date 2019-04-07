0a4eefc10e7aa304513e9020dcc7924810256908
jpguirado <43378763+jpguirado@users.noreply.github.com>
2019-04-07 14:23:50 +0200

    Clase pokemon, funcionalidad basica de filtros de legendarios, tipo y generacion

--- pokedexMongo/src/main/java/com/example/demo/ControladorPrincipal.java
+++ pokedexMongo/src/main/java/com/example/demo/ControladorPrincipal.java
@@ -19,31 +19,76 @@ import com.mongodb.client.MongoDatabase;
 @Controller
 public class ControladorPrincipal {
 
+	 List<Pokemon> listaPokemonCompleta = new ArrayList<Pokemon>();
 	
 	@GetMapping("/")
-	 public String greeting(Model model,HttpServletRequest request) {
+	 public String controladorInicio(Model model,HttpServletRequest request) {
 		//db.InsertarArticulo();		
 		
 		 MongoClient client = new MongoClient();
 	     MongoDatabase database = client.getDatabase("pokemonDB"); //Nombre de la base de datos
 	     MongoCollection<Document> collection = database.getCollection("pokemonC"); //Nombre de la coleccion
 
-	      	/*
-	        System.out.println("Find one:");
-	        Document first = collection.find().first();
-	        printJson(first);
-	        */
-	        
-	        
-	        
-	        
-	        System.out.println("Find all with into: ");
-	        List<Document> all = collection.find().into(new ArrayList<Document>());
-	        for (Document cur : all) {
-	            //printJson(cur);
-	        }
-	        
-	        model.addAttribute("pokemons", all);
+	     
+	     /*
+	      * Recuperamos los datos de la base de datos mongo y los introducimos en una lista de objetos de la clase
+	      * Pokemon para trabajar con ellos en posteriores filtros
+	      */
+	     List<Document> all = collection.find().into(new ArrayList<Document>());
+	     
+	     for (Document cur : all) {
+	    	 
+	    	 
+	    	 Pokemon pokemonAIntroducir = new Pokemon();
+	    	 
+	    	 pokemonAIntroducir.setHabilidades(cur.get("abilities").toString());
+	    	 pokemonAIntroducir.setAgainst_bug(cur.get("against_bug").toString());
+	    	 pokemonAIntroducir.setAgainst_dark(cur.get("against_dark").toString());
+	    	 pokemonAIntroducir.setAgainst_dragon(cur.get("against_dragon").toString());
+	    	 pokemonAIntroducir.setAgainst_electric(cur.get("against_electric").toString());
+	    	 pokemonAIntroducir.setAgainst_fairy(cur.get("against_fairy").toString());
+	    	 pokemonAIntroducir.setAgainst_fight(cur.get("against_fight").toString());
+	    	 pokemonAIntroducir.setAgainst_fire(cur.get("against_fire").toString());
+	    	 pokemonAIntroducir.setAgainst_flying(cur.get("against_flying").toString());
+	    	 pokemonAIntroducir.setAgainst_ghost(cur.get("against_ghost").toString());
+	    	 pokemonAIntroducir.setAgainst_grass(cur.get("against_grass").toString());
+	    	 pokemonAIntroducir.setAgainst_ground( cur.get("against_ground").toString());
+	    	 pokemonAIntroducir.setAgainst_ice( cur.get("against_ice").toString());
+	    	 pokemonAIntroducir.setAgainst_normal( cur.get("against_normal").toString());
+	    	 pokemonAIntroducir.setAgainst_poison(cur.get("against_poison").toString());
+	    	 pokemonAIntroducir.setAgainst_psychic(cur.get("against_psychic").toString());
+	    	 pokemonAIntroducir.setAgainst_rock(cur.get("against_rock").toString());
+	    	 pokemonAIntroducir.setAgainst_steel(cur.get("against_steel").toString());
+	    	 pokemonAIntroducir.setAgainst_water(cur.get("against_water").toString());
+	    	 pokemonAIntroducir.setAttack(cur.get("attack").toString());
+	    	 pokemonAIntroducir.setBase_egg_steps(cur.get("base_egg_steps").toString());
+	    	 pokemonAIntroducir.setBase_happiness(cur.get("base_happiness").toString());
+	    	 pokemonAIntroducir.setBase_total(cur.get("base_total").toString());
+	    	 pokemonAIntroducir.setCapture_rate(cur.get("capture_rate").toString());
+	    	 pokemonAIntroducir.setClassfication(cur.get("classfication").toString());
+	    	 pokemonAIntroducir.setDefense(cur.get("defense").toString());
+	    	 pokemonAIntroducir.setExperience_growth(cur.get("experience_growth").toString());
+	    	 pokemonAIntroducir.setHeight_m(cur.get("height_m").toString());
+	    	 pokemonAIntroducir.setHp(cur.get("hp").toString());
+	    	 pokemonAIntroducir.setJapanese_name(cur.get("japanese_name").toString());
+	    	 pokemonAIntroducir.setName(cur.get("name").toString());
+	    	 pokemonAIntroducir.setPercentage_male(cur.get("percentage_male").toString());
+	    	 pokemonAIntroducir.setPokedex_number(cur.get("pokedex_number").toString());
+	    	 pokemonAIntroducir.setSp_attack(cur.get("sp_attack").toString());
+	    	 pokemonAIntroducir.setSp_defense(cur.get("sp_defense").toString());
+	    	 pokemonAIntroducir.setSpeed(cur.get("speed").toString());
+	    	 pokemonAIntroducir.setType1(cur.get("type1").toString());
+	    	 pokemonAIntroducir.setType2(cur.get("type2").toString());
+	    	 pokemonAIntroducir.setWeight_kg(cur.get("weight_kg").toString());
+	    	 pokemonAIntroducir.setGeneration(cur.get("generation").toString());
+	    	 pokemonAIntroducir.setIs_legendary(cur.get("is_legendary").toString());
+	    	 
+	    	 listaPokemonCompleta.add(pokemonAIntroducir);
+	    	 
+	     }
+	     
+	     System.out.println("El tamaño de la lista es " + listaPokemonCompleta.size());   
+	     model.addAttribute("pokemons", listaPokemonCompleta);
 	        
 	        /*
 	        System.out.println("Find all with iteration: ");
@@ -64,4 +109,60 @@ public class ControladorPrincipal {
 		
 		return "prueba";
 	 }
+	
+	@GetMapping("/legendarios")
+	public String controladorLegendarios (Model model,HttpServletRequest request) {
+		
+		List<Pokemon> listaPokemonLegendarios = new ArrayList<Pokemon>();
+		
+		for (int i = 0; i<listaPokemonCompleta.size(); i++) {
+			if(listaPokemonCompleta.get(i).getIs_legendary().equals("1")) {
+				listaPokemonLegendarios.add(listaPokemonCompleta.get(i));
+			}
+		}
+		
+		model.addAttribute("pokemons", listaPokemonLegendarios);
+		
+		
+		return "prueba";
+	}
+	
+	@GetMapping("/generacion")
+	public String controladorGeneraciones (Model model,HttpServletRequest request) {
+		
+		
+		List<Pokemon> listaPokemonPorGeneracion = new ArrayList<Pokemon>();
+		
+		String generacion = "1";
+		
+		for (int i = 0; i<listaPokemonCompleta.size(); i++) {
+			if(listaPokemonCompleta.get(i).getGeneration().equals(generacion)) {
+				listaPokemonPorGeneracion.add(listaPokemonCompleta.get(i));
+			}
+		}
+		
+		model.addAttribute("pokemons", listaPokemonPorGeneracion);
+		
+		return "prueba";
+	}
+	
+	@GetMapping("/tipo")
+	public String controladorTipo (Model model,HttpServletRequest request) {
+		
+		
+		List<Pokemon> listaPokemonPorTipo = new ArrayList<Pokemon>();
+		
+		String tipo = "grass";
+		
+		for (int i = 0; i<listaPokemonCompleta.size(); i++) {
+			if(listaPokemonCompleta.get(i).getType1().equals(tipo) || listaPokemonCompleta.get(i).getType2().equals(tipo)) {
+				listaPokemonPorTipo.add(listaPokemonCompleta.get(i));
+			}
+		}
+		
+		model.addAttribute("pokemons", listaPokemonPorTipo);
+		
+		return "prueba";
+	}
+	
 }
---
+++ pokedexMongo/src/main/java/com/example/demo/Pokemon.java
@@ -0,0 +1,441 @@
+package com.example.demo;
+
+public class Pokemon {
+
+	private String habilidades;
+	
+	private String against_bug;
+	private String against_dark;
+	private String against_dragon;
+	private String against_electric;
+	private String against_fairy;
+	private String against_fight;
+	private String against_fire;
+	private String against_flying;
+	private String against_ghost;
+	private String against_grass;
+	private String against_ground;
+	private String against_ice;
+	private String against_normal;
+	private String against_poison;
+	private String against_psychic;
+	private String against_rock;
+	private String against_steel;
+	private String against_water;
+	
+	private String attack;
+	private String base_egg_steps;
+	private String base_happiness;
+	private String base_total;
+	private String capture_rate;
+	private String classfication;
+	private String defense;
+	private String experience_growth;
+	private String height_m;
+	private String hp;
+	
+	private String japanese_name;
+	private String name;
+	private String percentage_male;
+	private String pokedex_number;
+	private String sp_attack;
+	private String sp_defense;
+	private String speed;
+	
+	private String type1;
+	private String type2;
+	
+	private String weight_kg;
+	private String generation;
+	
+	private String is_legendary;
+
+	public Pokemon(String habilidades, String against_bug, String against_dark, String against_dragon,
+			String against_electric, String against_fairy, String against_fight, String against_fire,
+			String against_flying, String against_ghost, String against_grass, String against_ground,
+			String against_ice, String against_normal, String against_poison, String against_psychic,
+			String against_rock, String against_steel, String against_water, String attack, String base_egg_steps,
+			String base_happiness, String base_total, String capture_rate, String classfication, String defense,
+			String experience_growth, String height_m, String hp, String japanese_name, String name,
+			String percentage_male, String pokedex_number, String sp_attack, String sp_defense, String speed,
+			String type1, String type2, String weight_kg, String generation, String is_legendary) {
+		this.habilidades = habilidades;
+		this.against_bug = against_bug;
+		this.against_dark = against_dark;
+		this.against_dragon = against_dragon;
+		this.against_electric = against_electric;
+		this.against_fairy = against_fairy;
+		this.against_fight = against_fight;
+		this.against_fire = against_fire;
+		this.against_flying = against_flying;
+		this.against_ghost = against_ghost;
+		this.against_grass = against_grass;
+		this.against_ground = against_ground;
+		this.against_ice = against_ice;
+		this.against_normal = against_normal;
+		this.against_poison = against_poison;
+		this.against_psychic = against_psychic;
+		this.against_rock = against_rock;
+		this.against_steel = against_steel;
+		this.against_water = against_water;
+		this.attack = attack;
+		this.base_egg_steps = base_egg_steps;
+		this.base_happiness = base_happiness;
+		this.base_total = base_total;
+		this.capture_rate = capture_rate;
+		this.classfication = classfication;
+		this.defense = defense;
+		this.experience_growth = experience_growth;
+		this.height_m = height_m;
+		this.hp = hp;
+		this.japanese_name = japanese_name;
+		this.name = name;
+		this.percentage_male = percentage_male;
+		this.pokedex_number = pokedex_number;
+		this.sp_attack = sp_attack;
+		this.sp_defense = sp_defense;
+		this.speed = speed;
+		this.type1 = type1;
+		this.type2 = type2;
+		this.weight_kg = weight_kg;
+		this.generation = generation;
+		this.is_legendary = is_legendary;
+	}
+
+	public Pokemon() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getHabilidades() {
+		return habilidades;
+	}
+
+	public void setHabilidades(String habilidades) {
+		this.habilidades = habilidades;
+	}
+
+	public String getAgainst_bug() {
+		return against_bug;
+	}
+
+	public void setAgainst_bug(String against_bug) {
+		this.against_bug = against_bug;
+	}
+
+	public String getAgainst_dark() {
+		return against_dark;
+	}
+
+	public void setAgainst_dark(String against_dark) {
+		this.against_dark = against_dark;
+	}
+
+	public String getAgainst_dragon() {
+		return against_dragon;
+	}
+
+	public void setAgainst_dragon(String against_dragon) {
+		this.against_dragon = against_dragon;
+	}
+
+	public String getAgainst_electric() {
+		return against_electric;
+	}
+
+	public void setAgainst_electric(String against_electric) {
+		this.against_electric = against_electric;
+	}
+
+	public String getAgainst_fairy() {
+		return against_fairy;
+	}
+
+	public void setAgainst_fairy(String against_fairy) {
+		this.against_fairy = against_fairy;
+	}
+
+	public String getAgainst_fight() {
+		return against_fight;
+	}
+
+	public void setAgainst_fight(String against_fight) {
+		this.against_fight = against_fight;
+	}
+
+	public String getAgainst_fire() {
+		return against_fire;
+	}
+
+	public void setAgainst_fire(String against_fire) {
+		this.against_fire = against_fire;
+	}
+
+	public String getAgainst_flying() {
+		return against_flying;
+	}
+
+	public void setAgainst_flying(String against_flying) {
+		this.against_flying = against_flying;
+	}
+
+	public String getAgainst_ghost() {
+		return against_ghost;
+	}
+
+	public void setAgainst_ghost(String against_ghost) {
+		this.against_ghost = against_ghost;
+	}
+
+	public String getAgainst_grass() {
+		return against_grass;
+	}
+
+	public void setAgainst_grass(String against_grass) {
+		this.against_grass = against_grass;
+	}
+
+	public String getAgainst_ground() {
+		return against_ground;
+	}
+
+	public void setAgainst_ground(String against_ground) {
+		this.against_ground = against_ground;
+	}
+
+	public String getAgainst_ice() {
+		return against_ice;
+	}
+
+	public void setAgainst_ice(String against_ice) {
+		this.against_ice = against_ice;
+	}
+
+	public String getAgainst_normal() {
+		return against_normal;
+	}
+
+	public void setAgainst_normal(String against_normal) {
+		this.against_normal = against_normal;
+	}
+
+	public String getAgainst_poison() {
+		return against_poison;
+	}
+
+	public void setAgainst_poison(String against_poison) {
+		this.against_poison = against_poison;
+	}
+
+	public String getAgainst_psychic() {
+		return against_psychic;
+	}
+
+	public void setAgainst_psychic(String against_psychic) {
+		this.against_psychic = against_psychic;
+	}
+
+	public String getAgainst_rock() {
+		return against_rock;
+	}
+
+	public void setAgainst_rock(String against_rock) {
+		this.against_rock = against_rock;
+	}
+
+	public String getAgainst_steel() {
+		return against_steel;
+	}
+
+	public void setAgainst_steel(String against_steel) {
+		this.against_steel = against_steel;
+	}
+
+	public String getAgainst_water() {
+		return against_water;
+	}
+
+	public void setAgainst_water(String against_water) {
+		this.against_water = against_water;
+	}
+
+	public String getAttack() {
+		return attack;
+	}
+
+	public void setAttack(String attack) {
+		this.attack = attack;
+	}
+
+	public String getBase_egg_steps() {
+		return base_egg_steps;
+	}
+
+	public void setBase_egg_steps(String base_egg_steps) {
+		this.base_egg_steps = base_egg_steps;
+	}
+
+	public String getBase_happiness() {
+		return base_happiness;
+	}
+
+	public void setBase_happiness(String base_happiness) {
+		this.base_happiness = base_happiness;
+	}
+
+	public String getBase_total() {
+		return base_total;
+	}
+
+	public void setBase_total(String base_total) {
+		this.base_total = base_total;
+	}
+
+	public String getCapture_rate() {
+		return capture_rate;
+	}
+
+	public void setCapture_rate(String capture_rate) {
+		this.capture_rate = capture_rate;
+	}
+
+	public String getClassfication() {
+		return classfication;
+	}
+
+	public void setClassfication(String classfication) {
+		this.classfication = classfication;
+	}
+
+	public String getDefense() {
+		return defense;
+	}
+
+	public void setDefense(String defense) {
+		this.defense = defense;
+	}
+
+	public String getExperience_growth() {
+		return experience_growth;
+	}
+
+	public void setExperience_growth(String experience_growth) {
+		this.experience_growth = experience_growth;
+	}
+
+	public String getHeight_m() {
+		return height_m;
+	}
+
+	public void setHeight_m(String height_m) {
+		this.height_m = height_m;
+	}
+
+	public String getHp() {
+		return hp;
+	}
+
+	public void setHp(String hp) {
+		this.hp = hp;
+	}
+
+	public String getJapanese_name() {
+		return japanese_name;
+	}
+
+	public void setJapanese_name(String japanese_name) {
+		this.japanese_name = japanese_name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPercentage_male() {
+		return percentage_male;
+	}
+
+	public void setPercentage_male(String percentage_male) {
+		this.percentage_male = percentage_male;
+	}
+
+	public String getPokedex_number() {
+		return pokedex_number;
+	}
+
+	public void setPokedex_number(String pokedex_number) {
+		this.pokedex_number = pokedex_number;
+	}
+
+	public String getSp_attack() {
+		return sp_attack;
+	}
+
+	public void setSp_attack(String sp_attack) {
+		this.sp_attack = sp_attack;
+	}
+
+	public String getSp_defense() {
+		return sp_defense;
+	}
+
+	public void setSp_defense(String sp_defense) {
+		this.sp_defense = sp_defense;
+	}
+
+	public String getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(String speed) {
+		this.speed = speed;
+	}
+
+	public String getType1() {
+		return type1;
+	}
+
+	public void setType1(String type1) {
+		this.type1 = type1;
+	}
+
+	public String getType2() {
+		return type2;
+	}
+
+	public void setType2(String type2) {
+		this.type2 = type2;
+	}
+
+	public String getWeight_kg() {
+		return weight_kg;
+	}
+
+	public void setWeight_kg(String weight_kg) {
+		this.weight_kg = weight_kg;
+	}
+
+	public String getGeneration() {
+		return generation;
+	}
+
+	public void setGeneration(String generation) {
+		this.generation = generation;
+	}
+
+	public String getIs_legendary() {
+		return is_legendary;
+	}
+
+	public void setIs_legendary(String is_legendary) {
+		this.is_legendary = is_legendary;
+	}
+	
+	
+	
+}
+
+	
